148a62502225195f52d738ef132e895598e55780
Do Xuan Dinh <dinhdx@goquo.com>
2020-11-11 21:44:09 +0700

    resolve multi router method with once URL Solution

--- Core/Router.php
+++ Core/Router.php
@@ -4,36 +4,37 @@
 
 class Router{
     /**
-     * $routerList = ["url"=>$callback,"url"=> $callback]
-     * - $callback = ["callback"=> Controller@action, "method"=>"GET"]
-     * - $callback = ["callback"=> Controller@action, "method"=>"POST"]
+     * $routerList = [router1,router2]
+     * - $router = ["path"=>$url_define,"callback"=> Controller@action, "method"=>"GET"]
+     * - $router = ["path"=>$url_define,"callback"=> Controller@action, "method"=>"POST"]
      */
     static $routerList = [];
 
     public static function GET($url_define,$callback){
-        self::$routerList = array_merge(self::$routerList,array($url_define=>array("callback"=>$callback,"method"=>"GET")));
+    array_push(self::$routerList,array("path"=>$url_define,"callback"=>$callback,"method"=>"GET"));
 
     }
+
     public static function POST($url_define,$callback, array $params = []){
-        self::$routerList = array_merge(self::$routerList, array($url_define => array("callback" => $callback, "method" => "POST")));
+        array_push(self::$routerList, array("path"=>$url_define,"callback" => $callback, "method" => "POST"));
     }
     public static function PUT($url_define,$callback, array $params = []){
-        self::$routerList = array_merge(self::$routerList, array($url_define => array("callback" => $callback, "method" => "PUT")));
+        array_push(self::$routerList, array("path"=>$url_define,"callback" => $callback, "method" => "PUT"));
     }
     public static function DELETE($url_define,$callback, array $params = [])
     {
 
-        self::$routerList = array_merge(self::$routerList, array($url_define => array("callback" => $callback, "method" => "DELETE")));
+        array_push(self::$routerList, array("path"=>$url_define,"callback" => $callback, "method" => "DELETE"));
     }
     public static function PATCH($url_define,$callback, array $params = []){
         //
-        self::$routerList = array_merge(self::$routerList, array($url_define => array("callback" => $callback, "method" => "PATCH")));
+        array_push(self::$routerList, array("path"=>$url_define,"callback" => $callback, "method" => "PATCH"));
     }
 
-     /**
-     * $routerList = ["url"=>$callback,"url"=> $callback]
-     * - $callback = ["callback"=> Controller@action, "method"=>"GET"]
-     * - $callback = ["callback"=> Controller@action, "method"=>"POST"]
+    /**
+     * $routerList = [router1,router2]
+     * - $router = ["path"=>$url_define,"callback"=> Controller@action, "method"=>"GET"]
+     * - $router = ["path"=>$url_define,"callback"=> Controller@action, "method"=>"POST"]
      */
     protected static function loadController($callback,array $params=[]){
         // var_dump($callback);
@@ -77,20 +78,23 @@ class Router{
     {
         $request = new Request;
         static::parse($request);
-        foreach (self::$routerList as $url=>$callback) {
-            // if (strpos($request->url, "/public") === 0) {
-            //     echo "hehehe";
-            //     static::loadPublicFile($request->url);
-            // } else
-            if (!in_array($request->url,array_keys(self::$routerList))){
-                static::loadController(array("callback"=>"ErrorController@notFound","method"=>"GET"));
-            }
-            else if($request->url===$url){
+        if (!in_array($request->url,array_map(function($route){
+            return $route["path"];
+        },self::$routerList))){
+            static::loadController(array("callback"=>"ErrorController@notFound","method"=>"GET"));
+        }else{
+            foreach (self::$routerList as $index=>$router) {
+                if (($request->url===$router["path"]&&$request->method!=$router["method"])){
+                    continue;
+                }
+                else if($request->url===$router["path"]&&$request->method==$router["method"]){
+                    static::loadController($router,$request->params);
+                }
 
-                static::loadController($callback,$request->params);
             }
-
         }
+
+
     }
 
 
